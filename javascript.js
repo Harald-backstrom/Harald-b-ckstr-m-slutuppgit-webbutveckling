1e51c81602dd0bea2b8dd32254fcea875e267437
Harald <harald.backstrom@skola.taby.se>
2024-04-29 13:15:59 +0200

    saker

--- javascript.js
+++ javascript.js
@@ -1,24 +1,22 @@
 function toggleLanguage() {
-    var currentUrl = window.location.href;
+  var currentUrl = window.location.href;
 
-    if (currentUrl.includes("-en.html")) {
-        window.location.href = currentUrl.replace("-en.html", "-sv.html");
-    } else if (currentUrl.includes("-sv.html")) {
-        window.location.href = currentUrl.replace("-sv.html", "-en.html");
-    }
+  if (currentUrl.includes("-en.html")) {
+    window.location.href = currentUrl.replace("-en.html", "-sv.html");
+  } else if (currentUrl.includes("-sv.html")) {
+    window.location.href = currentUrl.replace("-sv.html", "-en.html");
+  }
 }
 
 function toggleMenu() {
-    var menuToggle = document.getElementById("menu-toggle");
-    var menuLinks = document.getElementById("menu-links");
+  var menuToggle = document.getElementById("menu-toggle");
+  var menuLinks = document.getElementById("menu-links");
 
-    menuToggle.classList.toggle("active"); // Toggle active class for styling
+  menuToggle.classList.toggle("active");
 
-    // Toggle the visibility of menu-links
-    if (menuLinks.style.display === "block") {
-        menuLinks.style.display = "none";
-    } else {
-        menuLinks.style.display = "block";
-    }
+  if (menuLinks.style.display === "block") {
+    menuLinks.style.display = "none";
+  } else {
+    menuLinks.style.display = "block";
+  }
 }
-
