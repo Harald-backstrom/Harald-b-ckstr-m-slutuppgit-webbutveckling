57a24eae064e03560948e5f18b2fe80055a60909
harald backstrom <harald.backstrom@skola.taby.se>
2024-05-03 19:17:47 +0200

    saker

--- mapbox.js
+++ mapbox.js
@@ -1,14 +1,15 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaGFycmViYXJyZSIsImEiOiJjbHR6d29pOG4wNTRvMmlzM2IwdXd4bnJkIn0.-oZ0a4L3ZQtzFXpoPjmqbA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGFycmViYXJyZSIsImEiOiJjbHR6d29pOG4wNTRvMmlzM2IwdXd4bnJkIn0.-oZ0a4L3ZQtzFXpoPjmqbA";
 
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [18.3853, 59.4059],
-    zoom: 10
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [18.33913, 59.42748],
+  zoom: 10,
 });
 
 var marker = new mapboxgl.Marker({
-color: "#FF0000"
+  color: "#FF0000",
 })
-.setLngLat([18.3853, 59.4059])
-.addTo(map);
+  .setLngLat([18.33913, 59.42748])
+  .addTo(map);
